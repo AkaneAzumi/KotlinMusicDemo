9c98c9bfb59af205640b70928fc585e6584c46b4
AkaneAzumi <1142681220@qq.com>
2019-10-25 18:02:31 +0800

    完善代码

--- app/src/main/java/com/wl/kotlinmusicdemo/MainActivity.kt
+++ app/src/main/java/com/wl/kotlinmusicdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.wl.kotlinmusicdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.wl.kotlinmusicdemo.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViewModel()
+    }
 
+    fun initViewModel(){
+        viewModel=ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 
 }
