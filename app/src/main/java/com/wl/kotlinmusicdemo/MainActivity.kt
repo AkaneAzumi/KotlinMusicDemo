a2d6e7e8dec205f4dddd0084bbfd84af43790bda
AkaneAzumi <1142681220@qq.com>
2019-10-25 17:24:30 +0800

    完善代码

--- app/src/main/java/com/wl/kotlinmusicdemo/MainActivity.kt
+++ app/src/main/java/com/wl/kotlinmusicdemo/MainActivity.kt
@@ -2,13 +2,17 @@ package com.wl.kotlinmusicdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.wl.kotlinmusicdemo.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private var viewModel:MainViewModel?=null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
     }
+
 }
--- app/src/main/java/com/wl/kotlinmusicdemo/ui/MusicListAdapter.kt
+++ app/src/main/java/com/wl/kotlinmusicdemo/ui/MusicListAdapter.kt
@@ -1,43 +1,74 @@
 package com.wl.kotlinmusicdemo.ui
 
+import android.content.Context
+import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.wl.kotlinmusicdemo.R
+import com.wl.kotlinmusicdemo.musicmodel.Music
 import kotlinx.android.synthetic.main.list_item.view.*
 
-class MusicListAdapter: RecyclerView.Adapter<MusicListAdapter.ViewHoler>() {
+class MusicListAdapter(musicList: MutableList<Music>,context: Context): RecyclerView.Adapter<MusicListAdapter.ViewHoler>(),View.OnClickListener {
+
+    private var onItemClickListener:OnItemClickListener?=null
+    private var musicList:MutableList<Music>?=null
+    private var context:Context?=null
+    init {
+        this.musicList=musicList
+        this.context=context
+    }
+
     override fun onBindViewHolder(holder: ViewHoler, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHoler {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    open class ViewHoler(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var title_textView=itemView.list_title
-        var artist_textView=itemView.list_artist
-        var size_textView=itemView.list_size
-        var time_textView=itemView.list_time
-        init {
-            itemView.setOnClickListener(View.OnClickListener {
-                if (it!=null){
-
-                }
+            holder.itemView.setOnClickListener(View.OnClickListener {
+                onItemClickListener?.OnItemClick(holder.itemView,holder.layoutPosition)
             })
+        if (musicList?.get(position)?.isChecked!!){
+            holder.artist_textView?.setTextColor(context!!.resources.getColor(R.color.main_color))
+            holder.time_textView?.setTextColor(context!!.resources.getColor(R.color.main_color))
+            holder.title_textView?.setTextColor(context!!.resources.getColor(R.color.main_color))
+            holder.size_textView?.setTextColor(context!!.resources.getColor(R.color.main_color))
+        }else{
+            holder.artist_textView?.setTextColor(Color.GRAY)
+            holder.time_textView?.setTextColor(Color.GRAY)
+            holder.title_textView?.setTextColor(Color.GRAY)
+            holder.size_textView?.setTextColor(Color.GRAY)
         }
 
     }
 
-    
+    override fun getItemCount(): Int {
+        return musicList!!.size
+    }
 
-    interface OnClickListener{
-        fun onClick()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHoler {
+        var v=View.inflate(context,R.layout.list_item,null)
+        v.setOnClickListener(this)
+
+
+        return  ViewHoler(v)
+    }
+
+    inner class ViewHoler(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var title_textView=itemView.list_title
+        var artist_textView=itemView.list_artist
+        var size_textView=itemView.list_size
+        var time_textView=itemView.list_time
+
+
+    }
+
+    private fun setOnItemClickListener(onItemClickListener: OnItemClickListener){
+        this.onItemClickListener=onItemClickListener
+    }
+     interface OnItemClickListener{
+        fun OnItemClick(v:View,position: Int)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        onItemClickListener?.OnItemClick(v!!,v!!.tag as Int)
     }
 
 }
